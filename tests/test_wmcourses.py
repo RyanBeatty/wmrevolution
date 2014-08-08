cd30f81cf32eed65effcbac5f7942c1f97f8eab6
RyanBeatty <rvbeatty@email.wm.edu>
2014-08-08 12:31:28 -0400

    added more tests. added military to standard time conversion

--- tests/test_wmcourses.py
+++ tests/test_wmcourses.py
@@ -1,8 +1,50 @@
 import unittest
 from wmrevolution import wmcourses, errors
+from bs4 import BeautifulSoup
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
 
 class WMCoursesTestCase(unittest.TestCase):
 
+	@classmethod
+	def setUpClass(cls):
+		cls.test_file = path.join(HERE, './fixtures/wmcourselist2014-08-08.html')
+
+
 	def test_bad_status_code(self):
 		with self.assertRaises(errors.WMRevolutionError):
-			wmcourses._retrieve_courselist_html(10000000)
+			wmcourses._grab_courselist_html(10000000)
+
+	def test_get_correct_course_fields(self):
+		course_fields = [
+			u'CRN',
+			u'COURSE ID',
+			u'COURSE ATTR',
+			u'TITLE',
+			u'INSTRUCTOR',
+			u'CRDT HRS',
+			u'MEET DAYS',
+			u'MEET TIMES',
+			u'PROJ ENR',
+			u'CURR ENR',
+			u'SEATS AVAIL',
+			u'STATUS',
+		]
+		with open(self.test_file) as f:
+			soup = BeautifulSoup(str(f.readlines()))
+			self.assertEqual(course_fields, wmcourses._grab_course_fields(soup))
+
+	def test_military_to_standard_bad_times(self):
+		self.assertEqual(wmcourses.format_time(u'2400-2430'), u'2400-2430')
+		self.assertEqual(wmcourses.format_time(u'240-243'), u'240-243')
+		self.assertEqual(wmcourses.format_time(u'1000-1060'), u'1000-1060')
+		self.assertEqual(wmcourses.format_time(u'1000-'), u'1000-')
+		self.assertEqual(wmcourses.format_time(u'-1000'), u'-1000')
+		self.assertEqual(wmcourses.format_time(u'-1000'), u'-1000')
+
+	def test_military_to_standard_time(self):
+		self.assertEqual(wmcourses.format_time(u'0000-0030'), u'12:00AM-12:30AM')
+		self.assertEqual(wmcourses.format_time(u'2300-0030'), u'11:00PM-12:30AM')
+		self.assertEqual(wmcourses.format_time(u'1159-1200'), u'11:59AM-12:00PM')
+		self.assertEqual(wmcourses.format_time(u'0100-0130'), u'01:00AM-01:30AM')
--- wmrevolution/wmcourses.py
+++ wmrevolution/wmcourses.py
@@ -13,6 +13,7 @@
 ##########################################################################
 
 import requests
+import datetime
 from argparse import ArgumentParser
 from lxml import html
 from bs4 import BeautifulSoup
@@ -21,8 +22,29 @@ from .errors import BadRequestError
 
 COURSELIST_URL = u'https://courselist.wm.edu/courseinfo/searchresults'
 
+def grab_courses(term_code, target=None):
+
+    soup = BeautifulSoup(_grab_courselist_html(term_code))
+    courses = _grab_raw_courses(soup)
+    if target:
+        courses = map(target, courses)
+    return courses
+
+def format_time(course_time):
+
+    def _to_datetime(raw_time):
+        return datetime.time(
+            hour=int(raw_time[0:2]), minute=int(raw_time[2:4]))
+
+    try:
+        times = course_time.split(u'-')
+        times = map(_to_datetime, times)
+        times = map(lambda t: t.strftime(u"%I:%M%p"), times)
+        return u'-'.join(times)
+    except ValueError:
+        return course_time
  
-def _retrieve_courselist_html(term_code):
+def _grab_courselist_html(term_code):
     payload = {
         u"term_code": unicode(term_code),
         u"term_subj": u"0",
@@ -38,11 +60,11 @@ def _retrieve_courselist_html(term_code):
         raise BadRequestError(u"can't access course list. check term code")
     return page.text
 
-def _get_course_fields(soup):
+def _grab_course_fields(soup):
     return [field.string.strip() for field in soup.find_all(u'th')]
 
-def grab_courses(soup):
-    course_fields = _get_course_fields(soup)
+def _grab_raw_courses(soup):
+    course_fields = _grab_course_fields(soup)
     courses = []
     course = {}
     count = 0
@@ -58,145 +80,38 @@ def grab_courses(soup):
 
     return courses
 
+# def grab_courses(term_code, target=None):
+
+#     soup = BeautifulSoup(_grab_courselist_html(term_code))
+#     courses = _grab_raw_courses(soup)
+#     if target:
+#         courses = map(target, courses)
+#     return courses
+
+# def _format_time(time):
+
+#     if time >= 1300:
+#         return _format(str(time - 1200)) + u"PM"
+#     elif time >= 1200:
+#         return _format(str(time)) + u"PM"
+#     else:
+#         return _format(str(time)) + u"AM"
+
+# def _format(time):
+
+#     str_len = len(time)
+
+#     if str_len == 4:
+#         return time[:2] + u':' + time[2:]
+#     else:
+#         return time[:1] + u':' + time[1:]
 
 
+# def grad_courses(term_code):
 
-# from bson.objectid import ObjectId
-
-# dictionary used to get key field for each course
-key_dict = {
-    '0': 'CRN',
-    '1': 'ATTRIBUTE',
-    '2': 'COURSE_ID',
-    '3': 'TITLE',
-    '4': 'INSTRUCTOR',
-    '5': 'CREDIT_HOURS',
-    '6': 'DAYS',
-    '7': 'TIMES',
-    '8': 'PROJ_ENR',
-    '9': 'CUR_ENR',
-    '10': 'AVAILABLE',
-    '11': 'STATUS'
-}
-
-# returns the string key associated with each field in a course entry
-# index: int between [0,11] used to get correct string key
-
-
-def switch(index):
-    return key_dict[str(index)]
-
-# converts militray time to standard time
-# time: time string formated in military time
-
-
-def _format_time(time):
-
-    if time >= 1300:
-        return _format(str(time - 1200)) + u"PM"
-    elif time >= 1200:
-        return _format(str(time)) + u"PM"
-    else:
-        return _format(str(time)) + u"AM"
-
-# formats the time field in each course entry
-
-
-def _format(time):
-
-    str_len = len(time)
-
-    if str_len == 4:
-        return time[:2] + u':' + time[2:]
-    else:
-        return time[:1] + u':' + time[1:]
-
-
-def get_course_list(term_code):
-
-    soup = BeautifulSoup(_retrieve_courselist_html(term_code))
-
-    field_count = 0         # used to keep track of current field
-    course_count = 0        # used to keep track of total number of courses
-    course = {}              # used to store each courses' fields
-
-    # course_fields = [
-    #     u'CRN',
-    #     u'ATTRIBUTE',
-    #     u'COURSE_ID',
-    #     u'TITLE',
-    #     u'INSTRUCTOR',
-    #     u'CREDIT_HOURS',
-    #     u'DAYS',
-    #     u'TIMES',
-    #     u'PROJ_ENR',
-    #     u'CUR_ENR',
-    #     u'AVAILABLE',
-    #     u'STATUS'
-    # ]
-    # course_fields = _get_course_fields(soup)
-    # courses = []
-    # course = {}
-    # count = 0
-    # for field in soup.find_all(u'td'):
-    #     if count == 12:
-    #         courses.append(course)
-    #         course = {}
-    #         count = 0
-    #     field_key = course_fields[count]
-    #     course[field_key] = field.text.strip()
-    #     count += 1
-
-    courses = grab_courses(soup)
-    return courses
-
-    
-
-
-
-    # # iterate over all entries in the open course list table
-    # for link in soup.find_all(u'td'):
-
-    #     # strip the text of extraneous characters
-    #     text = link.getText().strip()
-
-    #     # if the text is not whitespace/newline/tab, process text
-    #     if not text.isspace():
-    #         # get entry key string
-    #         key = switch(field_count)
-
-    #         # if we are processing a courses' time info, convert time to
-    #         # standard time
-    #         if key is 'TIMES':
-    #             time = text.split('-', 2)
-
-    #             try:
-    #                 time_a = convert2Standard(int(time[0]))
-    #                 time_b = convert2Standard(int(time[1]))
-
-    #                 entry[key] = time_a + '-' + time_b
-    #             except ValueError:
-    #                 entry[key] = text
-
-    #         # else just add the text to the course dict
-    #         else:
-    #             entry[key] = text
-
-    #         field_count += 1
-
-    #     # if we have built a full course entry
-    #     if field_count == 12:
-    #         # print the entry (change to insert to a database)
-    #         print entry
-
-    #         # reset the current field count and entry and increment the course
-    #         # count
-    #         field_count = 0
-    #         course_count += 1
-    #         entry = {}
-
-    # print 'number of courses: ' + str(course_count)
-    # print 'done'
+#     soup = BeautifulSoup(_grab_courselist_html(term_code))
+#     courses = _grab_raw_courses(soup)
+#     return courses
 
 
 if __name__ == '__main__':
@@ -209,6 +124,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print get_course_list(args.term_code)
+    print grab_courses(args.term_code)
     # _get_text(args.term_code)
     # main()
